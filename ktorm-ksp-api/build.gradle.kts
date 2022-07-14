e55b6812b518ddd0cab531bc1370b4bb91d15183
夜里的向日葵 <641571835@qq.com>
2022-07-14 10:32:13 +0800

    update tests

--- ktorm-ksp-api/build.gradle.kts
+++ ktorm-ksp-api/build.gradle.kts
@@ -11,37 +11,3 @@ dependencies {
 }
 
 configureMavenPublishing()
-
-for (jdkVendor in arrayOf(
-    JvmVendorSpec.ADOPTIUM,
-    JvmVendorSpec.ADOPTOPENJDK,
-    JvmVendorSpec.AZUL,
-    JvmVendorSpec.BELLSOFT,
-    JvmVendorSpec.HEWLETT_PACKARD,
-    JvmVendorSpec.IBM,
-    JvmVendorSpec.ORACLE,
-    JvmVendorSpec.SAP,
-)) {
-    for (majorVersion in arrayOf(8, 11, 17)) {
-        val jdkTest = tasks.register<Test>("testJdk $jdkVendor$majorVersion") {
-            val javaToolchains = project.extensions.getByType(JavaToolchainService::class)
-            javaLauncher.set(javaToolchains.launcherFor {
-                languageVersion.set(JavaLanguageVersion.of(majorVersion))
-                vendor.set(jdkVendor)
-            })
-
-            description = "Runs the test suite on JDK $jdkVendor$majorVersion"
-            group = LifecycleBasePlugin.VERIFICATION_GROUP
-
-            // Copy inputs from normal Test task.
-            val testTask = tasks.getByName<Test>("test")
-            classpath = testTask.classpath
-            testClassesDirs = testTask.testClassesDirs
-
-        }
-        tasks.named<Test>("test").configure {
-            dependsOn(jdkTest)
-        }
-    }
-}
-
